8fcef3dd7200b8dc43f92d351f7197c827580b3e
sprucetonye <waribosamuel@gmail.com>
2023-11-09 06:11:09 +0100

    second commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# A scratch repo on GitHub.com
+Hello everyone
+I will be writing some DevOps scripts here
+but while we wait I want to test run some commmands
---
+++ fast.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hello World"
+echo "Welcome! please type your name for a proper introduction"
+read name
+echo "You are most welcome $name"
